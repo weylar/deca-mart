2c06f06378d7595ba5a6a12dc733014d11a5a114
weylar <idrisaminu861@gmail.com>
2020-02-11 15:26:44 +0100

    Fix bug

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -3,15 +3,16 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    database: 'hussng',
-    username: 'postgres',
-    password: 'modupeola1960',
-    host: '127.0.0.1',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
     dialect: 'postgres'
   },
 
   test: {
-    database: 'huss_test',
+    database: 'deca-mart_test',
     username: 'postgres',
     password: 'modupeola1960',
     host: '127.0.0.1',
@@ -24,6 +25,6 @@ module.exports = {
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql'
+    dialect: 'postgres'
   }
 };
